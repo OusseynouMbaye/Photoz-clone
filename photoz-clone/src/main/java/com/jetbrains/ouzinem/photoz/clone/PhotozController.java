ec46503bfd395679b186837592ea817d62af62d9
Ouzinem <ouzinem@hotmail.com>
2022-05-13 10:08:47 -0400

    change list photo for collection photo with Map

--- photoz-clone/src/main/java/com/jetbrains/ouzinem/photoz/clone/PhotozController.java
+++ photoz-clone/src/main/java/com/jetbrains/ouzinem/photoz/clone/PhotozController.java
@@ -1,14 +1,22 @@
 package com.jetbrains.ouzinem.photoz.clone;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class PhotozController {
 
-    private List<Photo> db = List.of(new Photo("1","hello.jpg"));
+    private Map<String, Photo> db = new HashMap<>(){{
+      put("1",new Photo("1","hello.jpg"));
+    }};
+//    private List<Photo> db = List.of(new Photo("1","hello.jpg"));
 
     @GetMapping("/")
     public String hello(){
@@ -16,8 +24,13 @@ public class PhotozController {
     }
 
     @GetMapping("/photoz")
-    public List<Photo> get(){
-
-        return db;
+    public Collection<Photo> get(){
+        return db.values();
+    }
+    @GetMapping("/photoz/{id}")
+    public Photo get(@PathVariable String id){
+        Photo photo = db.get(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
     }
 }
